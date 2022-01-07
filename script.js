3cd353c378c3cfdff4742fd7c7c747231387eb26
sbme23 <93839730+sbme23@users.noreply.github.com>
2022-01-07 21:16:55 +0000

    Added players styles

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const body1 = document.getElementById("test")
 body1.style.backgroundColor = "red"
 
 // Conenct 4 requires two players 
+
 // red vs yellow
+
 // It needs to be 6 grids wide and 7 grids high 
 
 // Start a new game
@@ -13,10 +15,15 @@ for(s = 0; s < 42; s++) {
     let gridDivs = document.createElement('div');
     gridDivs.className = 'slot';
     document.getElementsByClassName('game-grid')[0].appendChild(gridDivs);
-    gridDivs.innerHTML = `<p>Hi there! I am a slot</p>`
+    gridDivs.innerHTML = `<p>slot</p>`
+    console.dir(gridDivs)
+    
 }
 
 
+
+
+// Need to probably add an onclick to the slots 
 
 // To win, a player will need to get 4 colours in a row 
 
