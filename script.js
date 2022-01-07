aff1adc7754c6cc56fcd9182c86c94bbea81021e
sbme23 <93839730+sbme23@users.noreply.github.com>
2022-01-07 18:58:27 +0000

    Updated js
    Created a for loop to dynamically ad the divs for the connect 4 game slots.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const body1 = document.getElementById("test") 
-
 body1.style.backgroundColor = "red"
 
 // Conenct 4 requires two players 
@@ -8,12 +7,29 @@ body1.style.backgroundColor = "red"
 
 // Start a new game
 
+// Create a grid for the game, will create the divs with JS
+
+
+for( g = 0; g < 9; g++) {
+let gameGrid = document.querySelector(".game-grid")
+    
+document.addEventListener('DOMContentLoaded', () => {
+    let slot = document.createElement('div');
+    slot.innerHTML = '<p>Hi there!</p>';
+    slot.className = 'slot';
+ 
+    document.body.appendChild(slot);
+}, false);
+}
+
+
+
+
 // To win, a player will need to get 4 colours in a row 
 
 // if quare below is taking the next player can go on top
 
 // check for win on every click maybe? 
-
 
 // We'll need to keep score 
 
