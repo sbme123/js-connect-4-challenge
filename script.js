90b5ecd013c0623a0ea8657754377b5fb8e7f12c
sbme23 <93839730+sbme23@users.noreply.github.com>
2022-01-07 08:41:55 +0000

    Added js notes to plan out the development

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const body1 = document.getElementById("test") 
 
 body1.style.backgroundColor = "red"
+
+// Conenct 4 requires two players 
+// red vs yellow
+// It needs to be 6 grids wide and 7 grids high 
+
+// Start a new game
+
+// To win, a player will need to get 4 colours in a row 
+
+// if quare below is taking the next player can go on top
+
+// check for win on every click maybe? 
+
+
+// We'll need to keep score 
+
+// Restart the game 
